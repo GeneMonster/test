45c728766f5b3a5694c38f73f7454cc25548f079
GeneMonster <wanghongjun@genedock.com>
2016-01-27 14:22:32 -0800

    index script

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,15 @@
-index.js
+(function () {
+
+	function Index() {
+		console.log('Index init...');
+	};
+
+	Index.prototype.base = function () {
+		console.log('Base...');
+	};
+
+	var index = new Index();
+
+	index.base();
+	
+})();
